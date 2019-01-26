ab7f8fd1d844d91891531d4cde81b0841fd0e866
Sergey Larin <cerg2010cerg2010@mail.ru>
2019-01-26 10:41:50 +0300

    drm/tegra: rgb: Hacks for S6E63M0
    Pins DATA_ENABLE, H_SYNC, V_SYNC, PIXEL_CLOCK are low polarity,
    set this in the registers to make the panel working.
    
    Signed-off-by: Sergey Larin <cerg2010cerg2010@mail.ru>

--- drivers/gpu/drm/tegra/rgb.c
+++ drivers/gpu/drm/tegra/rgb.c
@@ -34,7 +34,7 @@ struct reg_entry {
 	unsigned long value;
 };
 
-static const struct reg_entry rgb_enable[] = {
+static struct reg_entry rgb_enable[] = {
 	{ DC_COM_PIN_OUTPUT_ENABLE(0),   0x00000000 },
 	{ DC_COM_PIN_OUTPUT_ENABLE(1),   0x00000000 },
 	{ DC_COM_PIN_OUTPUT_ENABLE(2),   0x00000000 },
@@ -105,6 +105,15 @@ static void tegra_rgb_encoder_enable(struct drm_encoder *encoder)
 	struct tegra_rgb *rgb = to_rgb(output);
 	u32 value;
 
+	/*
+	 * Temporal hack for S6E63M0
+	 *
+	 * Pins DATA_ENABLE, H_SYNC, V_SYNC, PIXEL_CLOCK are low polarity,
+	 * set this in the registers to make the panel working.
+	 */
+	if (of_machine_is_compatible("samsung,i927"))
+		rgb_enable[7].value = 0x00000100;
+
 	tegra_dc_write_regs(rgb->dc, rgb_enable, ARRAY_SIZE(rgb_enable));
 
 	value = DE_SELECT_ACTIVE | DE_CONTROL_NORMAL;
@@ -112,25 +121,12 @@ static void tegra_rgb_encoder_enable(struct drm_encoder *encoder)
 
 	/* configure H- and V-sync and pixel clock signal polarities */
 	value = tegra_dc_readl(rgb->dc, DC_COM_PIN_OUTPUT_POLARITY(1));
-
-	if (mode->flags & DRM_MODE_FLAG_PHSYNC)
-		value &= ~LHS_OUTPUT_POLARITY_LOW;
-
-	if (mode->flags & DRM_MODE_FLAG_NHSYNC)
-		value |= LHS_OUTPUT_POLARITY_LOW;
-
-	if (mode->flags & DRM_MODE_FLAG_PVSYNC)
-		value &= ~LVS_OUTPUT_POLARITY_LOW;
-
-	if (mode->flags & DRM_MODE_FLAG_NVSYNC)
+	value &= ~LVS_OUTPUT_POLARITY_LOW;
+	value &= ~LHS_OUTPUT_POLARITY_LOW;
+	if (of_machine_is_compatible("samsung,i927")) {
 		value |= LVS_OUTPUT_POLARITY_LOW;
-
-	if (connector->display_info.bus_flags & DRM_BUS_FLAG_PIXDATA_DRIVE_POSEDGE)
-		value &= ~LSC0_OUTPUT_POLARITY_LOW;
-
-	if (connector->display_info.bus_flags & DRM_BUS_FLAG_PIXDATA_DRIVE_NEGEDGE)
-		value |= LSC0_OUTPUT_POLARITY_LOW;
-
+		value |= LHS_OUTPUT_POLARITY_LOW;
+	}
 	tegra_dc_writel(rgb->dc, value, DC_COM_PIN_OUTPUT_POLARITY(1));
 
 	/* configure DE signal polarities */
