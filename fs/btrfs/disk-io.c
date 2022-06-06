4ece4bc982dbf93ad4fd1773f51bd491554d03c9
Matthew Wilcox (Oracle) <willy@infradead.org>
2022-06-06 09:22:19 -0400

    btrfs: Convert btree_migratepage to migrate_folio
    Use a folio throughout this function.  migrate_page() will be converted
    later.
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Acked-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -469,7 +469,7 @@ static int btree_migrate_folio(struct address_space *mapping,
 	if (folio_get_private(src) &&
 	    !filemap_release_folio(src, GFP_KERNEL))
 		return -EAGAIN;
-	return migrate_folio(mapping, dst, src, mode);
+	return migrate_page(mapping, &dst->page, &src->page, mode);
 }
 #else
 #define btree_migrate_folio NULL
