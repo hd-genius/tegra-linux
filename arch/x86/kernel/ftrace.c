d62dd2d63c635f38529f33adc270e75e04c5a85a
Peter Zijlstra <peterz@infradead.org>
2022-10-25 21:39:47 +0200

    x86/ftrace: Remove SYSTEM_BOOTING exceptions
    Now that text_poke is available before ftrace, remove the
    SYSTEM_BOOTING exceptions.
    
    Specifically, this cures a W+X case during boot.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20221025201057.945960823@infradead.org

--- arch/x86/kernel/ftrace.c
+++ arch/x86/kernel/ftrace.c
@@ -423,7 +423,10 @@ create_trampoline(struct ftrace_ops *ops, unsigned int *tramp_size)
 	/* ALLOC_TRAMP flags lets us know we created it */
 	ops->flags |= FTRACE_OPS_FL_ALLOC_TRAMP;
 
-	set_memory_rox((unsigned long)trampoline, npages);
+	set_vm_flush_reset_perms(trampoline);
+
+	set_memory_ro((unsigned long)trampoline, npages);
+	set_memory_x((unsigned long)trampoline, npages);
 	return (unsigned long)trampoline;
 fail:
 	tramp_free(trampoline);
