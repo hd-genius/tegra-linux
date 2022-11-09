fcd017c4fdc4b1579059467b64e326c07cad666c
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2022-11-09 19:51:35 +0300

    x86/mm/iommu/sva: Make LAM and SVA mutually exclusive
    IOMMU and SVA-capable devices know nothing about LAM and only expect
    canonical addresses. An attempt to pass down tagged pointer will lead
    to address translation failure.
    
    By default do not allow to enable both LAM and use SVA in the same
    process.
    
    The new ARCH_FORCE_TAGGED_SVA arch_prctl() overrides the limitation.
    By using the arch_prctl() userspace takes responsibility to never pass
    tagged address to the device.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Reviewed-by: Ashok Raj <ashok.raj@intel.com>
    Reviewed-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Link: https://lore.kernel.org/all/20221109165140.9137-12-kirill.shutemov%40linux.intel.com

--- arch/x86/include/asm/mmu.h
+++ arch/x86/include/asm/mmu.h
@@ -9,13 +9,11 @@
 #include <linux/bits.h>
 
 /* Uprobes on this MM assume 32-bit code */
-#define MM_CONTEXT_UPROBE_IA32		0
+#define MM_CONTEXT_UPROBE_IA32		BIT(0)
 /* vsyscall page is accessible on this MM */
-#define MM_CONTEXT_HAS_VSYSCALL		1
-/* Do not allow changing LAM mode */
-#define MM_CONTEXT_LOCK_LAM		2
+#define MM_CONTEXT_HAS_VSYSCALL		BIT(1)
 /* Allow LAM and SVA coexisting */
-#define MM_CONTEXT_FORCE_TAGGED_SVA	3
+#define MM_CONTEXT_FORCE_TAGGED_SVA	BIT(2)
 
 /*
  * x86 has arch-specific MMU state beyond what lives in mm_struct.
