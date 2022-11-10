249a320369f378f28a920273ec10fe4d04611196
Miklos Szeredi <mszeredi@redhat.com>
2022-11-10 15:46:33 +0100

    fuse: optional supplementary group in create requests
    Permission to create an object (create, mkdir, symlink, mknod) needs to
    take supplementary groups into account.
    
    Add a supplementary group request extension.  This can contain an arbitrary
    number of group IDs and can be added to any request.  This extension is not
    added to any request by default.
    
    Add FUSE_CREATE_SUPP_GROUP init flag to enable supplementary group info in
    creation requests.  This adds just a single supplementary group that
    matches the parent group in the case described above.  In other cases the
    extension is not added.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -204,6 +204,8 @@
  *  - add total_extlen to fuse_in_header
  *  - add FUSE_MAX_NR_SECCTX
  *  - add extension header
+ *  - add FUSE_EXT_GROUPS
+ *  - add FUSE_CREATE_SUPP_GROUP
  */
 
 #ifndef _LINUX_FUSE_H
@@ -367,7 +369,6 @@ struct fuse_file_lock {
  * FUSE_HAS_INODE_DAX:  use per inode DAX
  * FUSE_CREATE_SUPP_GROUP: add supplementary group info to create, mkdir,
  *			symlink and mknod (single group that matches parent)
- * FUSE_HAS_EXPIRE_ONLY: kernel supports expiry-only entry invalidation
  */
 #define FUSE_ASYNC_READ		(1 << 0)
 #define FUSE_POSIX_LOCKS	(1 << 1)
@@ -405,7 +406,6 @@ struct fuse_file_lock {
 #define FUSE_SECURITY_CTX	(1ULL << 32)
 #define FUSE_HAS_INODE_DAX	(1ULL << 33)
 #define FUSE_CREATE_SUPP_GROUP	(1ULL << 34)
-#define FUSE_HAS_EXPIRE_ONLY	(1ULL << 35)
 
 /**
  * CUSE INIT request/reply flags
@@ -514,10 +514,12 @@ struct fuse_file_lock {
 /**
  * extension type
  * FUSE_MAX_NR_SECCTX: maximum value of &fuse_secctx_header.nr_secctx
+ * FUSE_EXT_GROUPS: &fuse_supp_groups extension
  */
 enum fuse_ext_type {
 	/* Types 0..31 are reserved for fuse_secctx_header */
 	FUSE_MAX_NR_SECCTX	= 31,
+	FUSE_EXT_GROUPS		= 32,
 };
 
 enum fuse_opcode {
@@ -1076,6 +1078,16 @@ struct fuse_secctx_header {
 struct fuse_ext_header {
 	uint32_t	size;
 	uint32_t	type;
+};
+
+/**
+ * struct fuse_supp_groups - Supplementary group extension
+ * @nr_groups: number of supplementary groups
+ * @groups: flexible array of group IDs
+ */
+struct fuse_supp_groups {
+	uint32_t	nr_groups;
+	uint32_t	groups[];
 };
 
 #endif /* _LINUX_FUSE_H */
