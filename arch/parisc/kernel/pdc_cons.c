e630b6b8776bc17dc946226b8ba3644ee0fff996
Helge Deller <deller@gmx.de>
2022-11-26 21:35:29 +0100

    parisc: Drop duplicate kgdb_pdc console in pdc_cons.c
    The kgdb console is already registered in kgdb.c, so the duplicate code
    can be dropped.
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/kernel/pdc_cons.c
+++ arch/parisc/kernel/pdc_cons.c
@@ -8,7 +8,6 @@
 #include <linux/console.h>
 #include <linux/init.h>
 #include <linux/serial_core.h>
-#include <linux/kgdb.h>
 #include <asm/page.h>		/* for PAGE0 */
 #include <asm/pdc.h>		/* for iodc_call() proto and friends */
 
@@ -20,27 +19,6 @@ static void pdc_console_write(struct console *co, const char *s, unsigned count)
 		i += pdc_iodc_print(s + i, count - i);
 	} while (i < count);
 }
-
-#ifdef CONFIG_KGDB
-static int kgdb_pdc_read_char(void)
-{
-	int c = pdc_iodc_getc();
-
-	return (c <= 0) ? NO_POLL_CHAR : c;
-}
-
-static void kgdb_pdc_write_char(u8 chr)
-{
-	/* no need to print char as it's shown on standard console */
-	/* pdc_iodc_print(&chr, 1); */
-}
-
-static struct kgdb_io kgdb_pdc_io_ops = {
-	.name = "kgdb_pdc",
-	.read_char = kgdb_pdc_read_char,
-	.write_char = kgdb_pdc_write_char,
-};
-#endif
 
 static int __init pdc_earlycon_setup(struct earlycon_device *device,
 				     const char *opt)
@@ -54,10 +32,6 @@ static int __init pdc_earlycon_setup(struct earlycon_device *device,
 	earlycon_console = device->con;
 	earlycon_console->write = pdc_console_write;
 	device->port.iotype = UPIO_MEM32BE;
-
-#ifdef CONFIG_KGDB
-	kgdb_register_io_module(&kgdb_pdc_io_ops);
-#endif
 
 	return 0;
 }
