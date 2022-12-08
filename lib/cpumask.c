353e014fe845fbfc0cc7f5798d09a85e2622da03
Yury Norov <yury.norov@gmail.com>
2022-12-08 10:31:00 -0800

    cpumask: improve on cpumask_local_spread() locality
    Switch cpumask_local_spread() to use newly added sched_numa_find_nth_cpu(),
    which takes into account distances to each node in the system.
    
    For the following NUMA configuration:
    
    root@debian:~# numactl -H
    available: 4 nodes (0-3)
    node 0 cpus: 0 1 2 3
    node 0 size: 3869 MB
    node 0 free: 3740 MB
    node 1 cpus: 4 5
    node 1 size: 1969 MB
    node 1 free: 1937 MB
    node 2 cpus: 6 7
    node 2 size: 1967 MB
    node 2 free: 1873 MB
    node 3 cpus: 8 9 10 11 12 13 14 15
    node 3 size: 7842 MB
    node 3 free: 7723 MB
    node distances:
    node   0   1   2   3
      0:  10  50  30  70
      1:  50  10  70  30
      2:  30  70  10  50
      3:  70  30  50  10
    
    The new cpumask_local_spread() traverses cpus for each node like this:
    
    node 0:   0   1   2   3   6   7   4   5   8   9  10  11  12  13  14  15
    node 1:   4   5   8   9  10  11  12  13  14  15   0   1   2   3   6   7
    node 2:   6   7   0   1   2   3   8   9  10  11  12  13  14  15   4   5
    node 3:   8   9  10  11  12  13  14  15   4   5   6   7   0   1   2   3
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Acked-by: Tariq Toukan <tariqt@nvidia.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Peter Lafreniere <peter@n8pjl.ca>

--- lib/cpumask.c
+++ lib/cpumask.c
@@ -145,12 +145,16 @@ unsigned int cpumask_local_spread(unsigned int i, int node)
 	/* Wrap: we always want a cpu. */
 	i %= num_online_cpus();
 
-	cpu = (node == NUMA_NO_NODE) ?
-		cpumask_nth(i, cpu_online_mask) :
-		sched_numa_find_nth_cpu(cpu_online_mask, i, node);
-
-	WARN_ON(cpu >= nr_cpu_ids);
-	return cpu;
+	if (node == NUMA_NO_NODE) {
+		cpu = cpumask_nth(i, cpu_online_mask);
+		if (cpu < nr_cpu_ids)
+			return cpu;
+	} else {
+		cpu = sched_numa_find_nth_cpu(cpu_online_mask, i, node);
+		if (cpu < nr_cpu_ids)
+			return cpu;
+	}
+	BUG();
 }
 EXPORT_SYMBOL(cpumask_local_spread);
 
